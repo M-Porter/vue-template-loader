69664d2bfb84f0f3780089e9dffc29087197f6fd
Matt Porter <mporter@squareup.com>
2019-03-21 10:40:12 -0700

    use curl instead of guzzle so we get rid of deps

--- config/vue_loader.php
+++ config/vue_loader.php
@@ -1,6 +1,5 @@
 <?php
 
 return [
-    'enabled' => env('VUE_LOADER_ENABLED', true),
     'webpack_endpoint' => env('VUE_LOADER_WEBPACK_ENDPOINT', 'https://localhost:8000'),
 ];
--- src/Loader.php
+++ src/Loader.php
@@ -2,8 +2,6 @@
 
 namespace MPorter\VueTemplateLoader;
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Log;
 
@@ -21,6 +19,7 @@ class Loader
     {
         if (App::isLocal()) {
             $dir = resource_path('views/vue');
+
             if (!is_dir($dir)) {
                 mkdir($dir, 0755, true);
             }
@@ -37,29 +36,36 @@ class Loader
      * @param string $filepath
      * @return void
      */
-    public static function saveTemplate(string $name, string $filepath)
+    private static function saveTemplate(string $name, string $filepath)
     {
-        $tmpResource = fopen('php://temp', 'w');
+        $ch = curl_init(self::buildUrl($name));
 
-        $client = new Client([
-            'base_uri' => config('vue_loader.webpack_endpoint'),
-        ]);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-        try {
-            $response = $client->request('get', "/${name}.html", [
-                'sink' => $tmpResource,
-                'verify' => false,
-            ]);
-            if ($response->getStatusCode() === 200) {
-                file_put_contents($filepath, $tmpResource);
-                fclose($tmpResource);
-            }
-        } catch (GuzzleException $e) {
+        $result = curl_exec($ch);
+        $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        curl_close($ch);
+
+        if ($result !== false && $statusCode === 200) {
+            file_put_contents($filepath, $result);
+        } else {
             Log::notice('[VueLoader] Failed saving stream.', [
-                'base_uri' => config('vue_loader.webpack_endpoint'),
+                'uri' => config('vue_loader.webpack_endpoint'),
                 'template' => $name,
                 'filepath' => $filepath,
             ]);
         }
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    private static function buildUrl(string $name): string
+    {
+        $base = rtrim(config('vue_loader.webpack_endpoint'), '/');
+        return "{$base}/${name}.html";
     }
 }
